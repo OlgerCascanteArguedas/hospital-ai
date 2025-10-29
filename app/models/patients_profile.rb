25598cc6691719286749fd093c02add2dfb1c941
carlos parra <u2204739@outlook.com>
2025-10-28 20:31:14 -0500

    patienents model created

--- app/models/patients_profile.rb
+++ app/models/patients_profile.rb
@@ -1,3 +1,10 @@
 class PatientsProfile < ApplicationRecord
   belongs_to :user
+  has_many :chats #pendiente por revisar dependiendo el nombre que plger utilice
+
+  validates :gender, :age
+  validates :height, :weight presence: true
+  validates :allergies, :conditions, :medication allow_blank: true
+
+
 end
