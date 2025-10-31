27d6fc183391ffe1749213494d015a7d8d36792e
Olger Cascante <olgerca100@gmail.com>
2025-10-30 19:58:22 -0600

    Avance Olger

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,11 @@
 Rails.application.routes.draw do
-  get 'patients_profiles/show'
-  get 'patients_profiles/edit'
-  get 'patients_profiles/update'
-  root "home#index"
-
-  devise_for :users
+   devise_for :users
 
   get "dashboard", to: "dashboard#show"
 
   resource :patients_profile, only: %i[show edit update]
   resources :chats, only: %i[index show create] do
+    post :ask, on: :member
     resources :messages, only: %i[index create]
   end
   resources :analyses, only: %i[index new create show]
